30a930d173c10a1bd7b95da674de731cd4cf5aa4
JC Marin <jcmarin@b1it.com>
2022-03-27 16:20:49 -0400

    CRUD ops setup.

---
+++ Port-web/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Port_web.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
