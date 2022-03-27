521b85ddebc5dad0a4a32040c70c9898857e420d
JC Marin <jcmarin@b1it.com>
2022-03-27 18:16:26 -0400

    Create changes

--- Port-web/Pages/Categories/Create.cshtml.cs
+++ Port-web/Pages/Categories/Create.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Port_web.Model;
 
 namespace Port_web.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        public Category Category { get; set; }
         public void OnGet()
         {
 
