33a0c5967e63c54a04c57dfa53e0f46cf34aacff
JC Marin <jcmarin@b1it.com>
2022-03-29 15:08:50 -0400

    Changes in relationships

---
+++ Port-web/Migrations/20220329190242_ProductKey.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Port_web.Migrations
+{
+    public partial class ProductKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FromPurchase",
+                table: "Product");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SKU",
+                table: "Product",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SKU",
+                table: "Product");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FromPurchase",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- Port-web/Model/Product.cs
+++ Port-web/Model/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Port_web.Model
 {
@@ -10,10 +11,12 @@ namespace Port_web.Model
         public string Name { get; set; }
         [Required]
         [Display(Name="Category Id")]
+        [ForeignKey("Category")]
         public int CategoryId { get; set; }
         [Required]
-        [Display(Name = "From Purchase")]
-        public int FromPurchase { get; set; }
+        [Display(Name = "SKU")]
+        [MaxLength(50, ErrorMessage = "SKU has to be shorter than 50 characters"),MinLength(5,ErrorMessage ="SKU has to be longer than 5 characters")]
+        public string SKU { get; set; }
         [Required]
         public double Cost { get; set; }
         [Required]
@@ -21,7 +24,7 @@ namespace Port_web.Model
         public double SellingPrice { get; set; }
         [Required]
         [Display(Name = "Discount %")]
-        [Range(1,99)]
+        [Range(1,99,ErrorMessage ="Must be between 1 and 99")]
         public int DiscountPercentage { get; set; }
 
 
