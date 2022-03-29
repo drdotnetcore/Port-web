8a28f0526e55445932a5fdc21aafc86e044d8745
JC Marin <jcmarin@b1it.com>
2022-03-29 19:33:02 -0400

    Foreign key changes

--- Port-web/Migrations/20220329232428_Fkeychangesproduct-category.Designer.cs
+++ Port-web/Migrations/20220329232428_Fkeychangesproduct-category.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Port_web.Data;
 
@@ -10,9 +11,10 @@ using Port_web.Data;
 namespace Port_web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220329232428_Fkeychangesproduct-category")]
+    partial class Fkeychangesproductcategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
