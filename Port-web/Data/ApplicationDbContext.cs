f49929505f6691ee14f7f0b6ac6d3d586701225d
JC Marin <jcmarin@b1it.com>
2022-03-30 11:18:15 -0400

    Identity Framework Scafolding.

--- Port-web/Data/ApplicationDbContext.cs
+++ Port-web/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Port_web.Model;
 
 namespace Port_web.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- Port-web/Program.cs
+++ Port-web/Program.cs
@@ -1,18 +1,31 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Port_web.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
 string strConnString = builder.Configuration["ConnString"];
 // Add services to the container.
-    builder.Services.AddRazorPages();
+//Authentication: Cookie options.
+
+
+
+builder.Services.AddRazorPages();
+/*----------------------------------JCMARIN--------------------------------------------------
 //The application requires the connection string to be a variable inside "user secrets" so that the 
-//connection string isn't exposed.
+//connection string isn't exposed. To use the secrets uncomment line below.  If using environment variables
+//then get it from the builder.Configuration settings above instead.
+------------------------------------JCMARIN--------------------------------------------------*/
 
-    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-        //builder.Configuration.GetConnectionString("ConnString")
-        strConnString
-        ));
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+         //builder.Configuration.GetConnectionString("ConnString")
+         strConnString
+        )); ;
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();builder.Services.AddDbContext<ApplicationDbContext>(options =>
+   // options.UseSqlServer(connectionString));
+   options.UseSqlServer(strConnString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +41,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
